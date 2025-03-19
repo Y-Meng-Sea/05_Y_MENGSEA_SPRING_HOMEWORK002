0604e6618050273f78d5f12d62cae298680fa845
mengsea <mrmengsea@gmail.com>
2025-03-19 13:35:52 +0700

    init project

---
+++ src/main/java/org/example/springboothomework02/SpringBootHomework02Application.java
@@ -0,0 +1,13 @@
+package org.example.springboothomework02;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootHomework02Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootHomework02Application.class, args);
+    }
+
+}
